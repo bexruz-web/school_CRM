35669b5bee3a70d6ea1c5a524feb191e3778e075
bexruz <bexruzasqarov08@gmail.com>
2025-03-14 21:14:34 +0500

    startapp for users

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('leadership', 'Rahbariyat'),
+        ('teacher', 'O\'qituvchi'),
+        ('staff', 'Maktab jamoasi')
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='teacher')
+
+    def __str__(self):
+        return f"{self.username} - {self.get_role_display()}"
+
+
