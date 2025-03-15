6b4a4938fae370072a448fc353003a2de08fc877
bexruz <bexruzasqarov08@gmail.com>
2025-03-15 23:21:04 +0500

    small updated

--- config/urls.py
+++ config/urls.py
@@ -40,7 +40,7 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/v1/', include('school_management.urls')),
+    path('api/v1/', include('school_management.urls')),
 
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -50,6 +50,8 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('apiauth/', include('rest_framework.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
