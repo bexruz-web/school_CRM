ee71813f4000c7a595958c025925fc667eef9d6a
bexruz <bexruzasqarov08@gmail.com>
2025-03-15 23:19:56 +0500

    urls added

---
+++ school_management/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from school_management.views import LeaderShipViewSet, SchoolStaffViewSet
+
+router = DefaultRouter()
+router.register(r'leadership', LeaderShipViewSet)
+router.register(r'school-staff', SchoolStaffViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
