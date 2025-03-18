ad0a471c9cb91c47230bc88f361ca470a58a4164
bexruz <bexruzasqarov08@gmail.com>
2025-03-19 01:41:17 +0500

    added urls

--- school_management/urls.py
+++ school_management/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from school_management.views import LeaderShipViewSet, SchoolStaffViewSet, GroupViewSet
+from school_management.views import LeaderShipViewSet, SchoolStaffViewSet, GroupViewSet, SubjectViewSet, TeacherViewSet, StudentViewSet
 
 router = DefaultRouter()
 router.register(r'groups', GroupViewSet)
-router.register(r'leadership', LeaderShipViewSet)
+router.register(r'leaderships', LeaderShipViewSet)
 router.register(r'school-staff', SchoolStaffViewSet)
+router.register(r'teachers', TeacherViewSet)
+router.register(r'students', StudentViewSet)
+router.register(r'subjects', SubjectViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
