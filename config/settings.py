474967726b7e980db94f52cde949031c4fa5564e
bexruz <bexruzasqarov08@gmail.com>
2025-03-22 00:00:14 +0500

    Sqlite to PostgreSQl

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school_db',
+        'USER': 'school_admin',
+        'PASSWORD': 'admin_pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
