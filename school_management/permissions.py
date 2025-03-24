71c59e2b2f9168783d5aaaa537815f15a4c574ca
bexruz <bexruzasqarov08@gmail.com>
2025-03-24 23:10:07 +0500

    events for permission

--- school_management/permissions.py
+++ school_management/permissions.py
@@ -11,3 +11,15 @@ class IsSuperuserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_superuser
+
+
+class IsTeacherOrReadOnly(permissions.BasePermission):
+    """
+    Permission is granted if the user has role='teacher'.
+    Other users can only read (GET, HEAD, OPTIONS).
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return getattr(request.user, 'role', None) == 'teacher'
