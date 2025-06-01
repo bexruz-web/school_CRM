72195f3d0cf2d4099d59159a4803019077bf489b
bexruz <bexruzasqarov08@gmail.com>
2025-06-01 17:26:02 +0500

    update

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
         ('teacher', 'O\'qituvchi'),
         ('staff', 'Maktab jamoasi')
     )
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='teacher')
 
     def __str__(self):
